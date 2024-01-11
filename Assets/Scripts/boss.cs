75eba2ed39c2ee6430e33b9464fe8bbd22e942a8
LorenzoStancato <lorenzo.stancato@agictech.com>
2024-01-11 12:37:28 +0100

    feat: add roar animation and walk animation

---
+++ Assets/Scripts/boss.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class boss : MonoBehaviour
+{
+    public GameObject player;
+    public NavMeshAgent agent;
+
+    public LayerMask BossFlyingArea, whatIsPlayer;
+
+    public float sightRange, attackRange;
+
+    public bool playerInSightRange, playerInAttackRange;
+
+    public float timeBetweenAttacks;
+    public float playerDistance;
+    protected bool alreadyAttacked;
+
+    private bool canMove = false;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            GetComponent<Animator>().SetTrigger("Roar");
+            ChasePlayer();
+        }
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        transform.LookAt(player.transform);
+        // if (!gameObject.CompareTag("Goblin"))
+        transform.Rotate(0, 180, 0);
+        agent.SetDestination(player.transform.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        transform.LookAt(player.transform);
+        // if (!gameObject.CompareTag("Goblin"))
+        transform.Rotate(0, 180, 0);
+        agent.SetDestination(transform.position);
+        if (!alreadyAttacked && agent.remainingDistance < playerDistance)
+        {
+            //Attack code here
+            // GetComponent<Animator>().SetTrigger("Attack");
+            // alreadyAttacked = true;
+            Debug.Log("Boss Attack");
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    protected virtual void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //Draw attack and sight range
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+}
